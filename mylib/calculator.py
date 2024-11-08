a3ebf3942d79d1aba6ade04e1684371e4fa2aaa8
Jiaxin-Cindy <jiaxin.gao1997@gmail.com>
2024-11-08 13:49:29 -0500

    add pyspark environment

--- mylib/calculator.py
+++ mylib/calculator.py
@@ -2,19 +2,49 @@
 Calculations library
 """
 
-#var=
+import os
+import requests
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import when, col
 
-def add(a, b):
-    return a + b
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType
 
-
-def subtract(a, b):
-    return a - b
-
-
-def multiply(a, b):
-    return a * b
+LOG_FILE = "pyspark_output.md"
 
 
-def divide(a, b):
-    return a / b
+def log_output(operation, output, query=None):
+    """adds to a markdown file"""
+    with open(LOG_FILE, "a") as file:
+        file.write(f"The operation is {operation}\n\n")
+        if query:
+            file.write(f"The query is {query}\n\n")
+        file.write("The truncated output is: \n\n")
+        file.write(output)
+        file.write("\n\n")
+
+
+def start_spark(appName):
+    spark = SparkSession.builder.appName(appName).getOrCreate()
+    return spark
+
+
+def end_spark(spark):
+    spark.stop()
+    return "stopped spark session"
+
+
+def extract(
+    url="""
+    https://raw.githubusercontent.com/fivethirtyeight/data/refs/heads/master/murder_2016/murder_2015_final.csv 
+    """,
+    file_path="data/murder_2015_final.csv",
+    directory="data",
+):
+    """Extract a url to a file path"""
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    with requests.get(url) as r:
+        with open(file_path, "wb") as f:
+            f.write(r.content)
+
+    return file_path
